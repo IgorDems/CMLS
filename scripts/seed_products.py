f163db66020ed04e0809f1c3f74c19c44e5cc810
Your Name <you@example.com>
2026-03-20 02:14:44 -0500

    add seed job

---
+++ scripts/seed_products.py
@@ -0,0 +1,14 @@
+products = [
+    {
+        "pk": "PRODUCT#1",
+        "sk": "META",
+        "name": "MacBook Pro 14",
+        "price": 1999,
+    },
+    {
+        "pk": "PRODUCT#2",
+        "sk": "META",
+        "name": "iPhone 15",
+        "price": 999,
+    },
+]
